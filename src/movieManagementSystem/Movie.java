daf46533c808112e7432594b58b71b38e733b3c3
SpoonsTheUtensil <alexschmierbach@yahoo.com>
2021-01-23 12:40:53 -0600

    Update Movie.java
    Changes to whitespace and constructors

--- src/movieManagementSystem/Movie.java
+++ src/movieManagementSystem/Movie.java
@@ -14,78 +14,50 @@ public class Movie {
 	LocalDate movie_releaseDate, movie_receiveDate;
 	Status movie_status;
 	
-	public Movie() {
-		
-		movie_name = "";
-		movie_description = "";
-		movie_releaseDate = null;
-		movie_receiveDate = null;
-		movie_status = null;
-		
-	}
+	public Movie() {} // Default constructor
 	
 	public Movie(String name, String description) {
-		
+		movie_name = name;
+		movie_description = description;
 	}
 	
 	public void setMovieTitle(String title) {
-		
 		movie_name = title;
-		
 	}
 	
 	public void setMovieDesc(String desc) {
-		
 		movie_description = desc;
-		
 	}
 	
 	public void setMovieReleaseDate(LocalDate date) {
-		
 		movie_releaseDate = date;
-		
 	}
 	
 	public void setMovieRecieveDate(LocalDate date) {
-		
 		movie_receiveDate = date;
-		
 	}
 	
 	public void setMovieStats(Status status) {
-		
 		movie_status = status;
-		
 	}
 	
 	public String getMovieTitle() {
-		
 		return movie_name;
-		
 	}
 	
 	public String getMovieDesc() {
-		
 		return movie_description;
-		
 	}
 	
 	public LocalDate getMovieRelease() {
-		
 		return movie_releaseDate;
-		
 	}
 	
 	public LocalDate getMovieReceive() {
-		
 		return movie_receiveDate;
-		
 	}
 	
 	public Status getMovieStatus() {
-		
 		return movie_status;
-		
 	}
-	
 }
